859c059199b1f0d98074a3bd146d6d5f08690a79
Anirban Basak <jeetbasak2002@gmail.com>
2022-10-03 12:27:13 +0530

    03/10/2022

--- app/src/main/java/com/basakjeet08/calculator/MainActivity.kt
+++ app/src/main/java/com/basakjeet08/calculator/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         startingOfProgram()
     }
     private fun startingOfProgram(){
+        var output = 0
+
         val tvInput : TextView = findViewById(R.id.tvInput)
         val btnZero : Button = findViewById(R.id.btnZero)
         val btnOne : Button = findViewById(R.id.btnOne)
@@ -31,37 +33,51 @@ class MainActivity : AppCompatActivity() {
         val btnClear : Button = findViewById(R.id.btnClear)
 
         btnZero.setOnClickListener {
+            output = output*10 + 0
             tvInput.text = if(checkZero(tvInput)) "0" else "${tvInput.text}0"
         }
         btnOne.setOnClickListener {
+            output = output*10 + 1
             tvInput.text = if(checkZero(tvInput)) "1" else "${tvInput.text}1"
         }
         btnTwo.setOnClickListener {
+            output = output*10 + 2
             tvInput.text = if(checkZero(tvInput)) "2" else "${tvInput.text}2"
         }
         btnThree.setOnClickListener{
+            output = output*10 + 3
             tvInput.text = if(checkZero(tvInput)) "3" else "${tvInput.text}3"
         }
         btnFour.setOnClickListener{
+            output = output*10 + 4
             tvInput.text = if(checkZero(tvInput)) "4" else "${tvInput.text}4"
         }
         btnFive.setOnClickListener{
+            output = output*10 + 5
             tvInput.text = if(checkZero(tvInput)) "5" else "${tvInput.text}5"
         }
         btnSix.setOnClickListener{
+            output = output*10 + 6
             tvInput.text = if(checkZero(tvInput)) "6" else "${tvInput.text}6"
         }
         btnSeven.setOnClickListener{
+            output = output*10 + 7
             tvInput.text = if(checkZero(tvInput)) "7" else "${tvInput.text}7"
         }
         btnEight.setOnClickListener{
+            output = output*10 + 8
             tvInput.text = if(checkZero(tvInput)) "8" else "${tvInput.text}8"
         }
         btnNine.setOnClickListener{
+            output = output*10 + 9
             tvInput.text = if(checkZero(tvInput)) "9" else "${tvInput.text}9"
         }
         btnAdd.setOnClickListener{
             tvInput.text = "${tvInput.text}\n"
+        }
+
+        btnClear.setOnClickListener{
+            tvInput.text = "0"
         }
     }
     private fun checkZero(tvInput:TextView) : Boolean{
@@ -69,8 +85,8 @@ class MainActivity : AppCompatActivity() {
             return true
         return false
     }
-    private fun additionNumber() : Int{
+    private fun additionNumber(num:Int) : Int{
 
-        return 0
+        return num
     }
 }
